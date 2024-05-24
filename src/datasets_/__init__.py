f176b0d733299c56e974b625d3cc7eb01f68d7c2
changdaeoh <bnormal16@naver.com>
2024-05-24 16:10:14 +0900

    init

--- src/datasets_/__init__.py
+++ src/datasets_/__init__.py
@@ -1,6 +1,5 @@
 from .imagenet import *
 from .imagenetv2 import ImageNetV2
-from .imagenet_c import ImageNetC
 from .imagenet_a import ImageNetAValClasses, ImageNetA
 from .imagenet_r import ImageNetRValClasses, ImageNetR
 from .imagenet_sketch import ImageNetSketch
